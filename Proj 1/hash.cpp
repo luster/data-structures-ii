b2b0022355b115368ee3a292cbb6f6511bff5bd8
Ethan Lusterman <ethanlusterman@lusterdome.home>
2013-09-10 22:08:57 -0400

    started writing hash implementation

---
+++ Proj 1/hash.cpp
@@ -0,0 +1,12 @@
+#include "hash.h"
+#include <iostream>
+
+using namespace std;
+
+void hashTable::insert(const std::string &key, void *pv = NULL) {
+}
+
+int hashTable::hash(const std::string &key) {
+	int hash = 37;
+	
+}
