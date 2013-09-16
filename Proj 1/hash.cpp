b60e0b717a2e91f81836b46a401fae071dc96a1d
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-09-16 15:01:42 -0400

    update

--- Proj 1/hash.cpp
+++ Proj 1/hash.cpp
@@ -1,47 +1,55 @@
 #include <iostream>
 #include "hash.h"
+#include <vector>
+#include <string>
 
 using namespace std;
 
+int main() {
+    return 0;
+}
 
-void hashTable::hashTable(int size = 0) {
-
-	capacity = getPrime(size);
-
+hashTable::hashTable(int size) {
+    capacity = getPrime(size);
+    data.resize(capacity);
 }
 
 
-void hashTable::insert(const std::string &key, void *pv = NULL) {
-	// what
+int hashTable::insert(const std::string &key, void *pv) {
+    return 1;
+
 }
 
 
 int hashTable::hash(const std::string &key) {
-	
-	int hashValue = 0;
 
-	for (int i=0; i < key.length(); i++)
-		hashValue = 37*hashValue + key[i];
+    int hashValue = 0;
 
-	hashValue %= capacity;
+    for (int i=0; i < key.length(); i++)
+        hashValue = 37*hashValue + key[i];
 
-	return hashValue;
+    hashValue %= capacity;
+
+    return hashValue;
 }
 
 
-static unsigned int hashTable::getPrime(int size) {
+unsigned int hashTable::getPrime(int size) {
 
-	vector<int> primes;
+    vector<int> primes;
 
-	primes.push_back(100003);
-	primes.push_back(505919);
-	primes.push_back(1054007);
-	primes.push_back(2380481);
-	primes.push_back(4114741);
-	primes.push_back(6699197);
-	primes.push_back(8487821);
+    primes.push_back(100003);
+    primes.push_back(505919);
+    primes.push_back(1054007);
+    primes.push_back(2380481);
+    primes.push_back(4114741);
+    primes.push_back(6699197);
+    primes.push_back(8487821);
 
-	for (int i=0; i < primes.size(); i++)
-		if (primes[i] > size)
-			return primes[i];
+    for (int i=0; i < primes.size(); i++) {
+        if (primes[i] > size) {
+            return primes[i];
+        }
+    }
+    return 0;
 }
---
+++ Proj 1/test/testVector.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> data;
+    data.resize(10000);
+    cout << data.size() << endl;
+
+    return 0;
+}
