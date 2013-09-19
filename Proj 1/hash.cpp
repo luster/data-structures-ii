07c75b422d77957492e5ea02a1433cbb6c3fa004
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-09-19 19:57:22 -0400

    modified gitignore and removed some old cout statements

--- Proj 1/hash.cpp
+++ Proj 1/hash.cpp
@@ -8,23 +8,12 @@ using namespace std;
 int main() {
     hashTable testTable(10);
     int ins = testTable.insert("abcdefgh");
-    cout << ins << endl;
     int ins2 = testTable.insert("abcd");
-    cout << ins2 << endl;
     int ins3 = testTable.insert("dcba");
-    cout << ins3 << endl;
     int ins4 = testTable.insert("edcba");
-    cout << ins4 << endl;
-    //string key = "duh";
-    //bool test = testTable.contains(key);
-    //cout << "table contains key: " << test << endl;
-    //int insert = testTable.insert(key);
-    //cout << insert << endl;
-    //test = testTable.contains(key);
-    //cout << "table contains key: " << test << endl;
     bool x = testTable.datHash();
+
     cout << x << endl;
-    cout << ins << ins2 << ins3 << ins4 << endl;
 
     return 0;
 }
@@ -115,26 +104,24 @@ int hashTable::findPos(const std::string &key) {
 
 bool hashTable::rehash() {
     // new hash table, declare and resize
-    cout << "rehash 1" << endl;
     vector<hashItem> tmpData;
-    cout << "rehash 2" << endl;
     int newSize = getPrime(data.size());
-    cout << "rehash 3" << endl;
     tmpData.resize(newSize);
-    cout << "rehash 4" << endl;
     capacity = newSize;
-    cout << "rehash 5" << endl;
 
     // swap data and tmpData since class def changes data vector
     data.swap(tmpData);
     cout << "rehash 6" << endl;
 
-    cout << tmpData.size() << endl;
+    //cout << tmpData.size() << endl;
+    //cout << data.size() << endl;
     cout << tmpData[0].isOccupied << endl;
     for (int i=0; i < tmpData.size(); i++) {
         //cout << i << endl;
-        if (tmpData[i].isOccupied)
+        if (tmpData[i].isOccupied) {
             int ins = insert(tmpData[i].key);
+            cout << ins << endl;
+        }
     }
     cout << "done with rehash" << endl;
 
