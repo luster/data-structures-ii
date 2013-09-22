cc4a3d0e8ea5225742d47f7e73c5db7af66f39cf
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-09-22 19:39:22 -0400

    added testStr file

--- Proj 1/test/testStr.cpp
+++ Proj 1/test/testStr.cpp
@@ -5,7 +5,20 @@
 using namespace std;
 
 int main() {
-    string s;
 
     return 0;
+}
+
+
+void gen_random(char *s, const int len) {
+        static const char alphanum[] =
+                    "0123456789"
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                    "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i < len; ++i) {
+                        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+                            }
+
+                s[len] = 0;
 }
