a37f2547c070d0c5d5b66e1d1eb6a2ca65d54957
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-10-02 12:31:01 -0400

    updated heap header file

--- Proj 2/heap.h
+++ Proj 2/heap.h
@@ -10,30 +10,87 @@
 
 class heap {
 
-    public:
+  public:
 
-        // The constructor initializes the heap.
-        heap(int size = 0);
+    // The constructor allocates space for the nodes of the heap
+    // and the mapping (hash table) based on the specified capacity
+    heap(int capacity);
 
-    private:
+    //
+    // insert - Inserts a new node into the binary heap
+    //
+    // Inserts a node with the specified id string, key,
+    // and optionally a pointer. The key is used to
+    // determine the final position of the new node.
+    //
+    // Returns:
+    //   0 on success
+    //   1 if the heap is already filled to capacity
+    //   2 if a node with the given id already exists (but the heap
+    //     is not filled to capacity)
+    //
+    int insert(const std::string &id, int key, void *pv = NULL);
 
-        class node {
-            public:
-                // The id of this node
-                std::string id;
+    //
+    // setKey - set the key of the specified node to the specified value
+    //
+    // I have decided that the class should provide this member function
+    // instead of two separate increaseKey and decreaseKey functions.
+    //
+    // Returns:
+    //   0 on success
+    //   1 if a node with the given id does not exist
+    //
+    int setKey(const std::string &id, int key);
 
-                // The key of this node
-                int key;
+    //
+    // deleteMin - return the data associated with the smallest key
+    //             and delete that node from the binary heap
+    //
+    // If pId is supplied (i.e., it is not NULL), write to that address
+    // the id of the node being deleted. If pKey is supplied, write to
+    // that address the key of the node being deleted. If ppData is
+    // supplied, write to that address the associated void pointer.
+    //
+    // Returns:
+    //  0 on success
+    //  1 if the heap is empty
+    //
+    int deleteMin(std::string *pId = NULL, int *pKey = NULL, void *ppData = NULL);
 
-                // A pointer to the actual data
-                void *pData;
-        };
+    //
+    // remove - delete the node with the specified id from the binary heap
+    //
+    // If pKey is supplied, write to that address the key of the node
+    // being deleted. If ppData is supplied, write to that address the
+    // associated void pointer.
+    //
+    // Returns:
+    //  0 on success
+    //  1 if a node with the given id does not exist
+    //
+    int remove(const std::string &id, int *pKey = NULL, void *ppData = NULL);
 
-        void percolateUp(int posCur);
 
-        void percolateDown(int posCur);
+  private:
 
-        int getPos(node *pn);
+    class node {
+        public:
+            // The id of this node
+            std::string id;
+
+            // The key of this node
+            int key;
+
+            // A pointer to the actual data
+            void *pData;
+    };
+
+    void percolateUp(int posCur);
+
+    void percolateDown(int posCur);
+
+    int getPos(node *pn);
 };
 
 #endif //_HEAP_H
