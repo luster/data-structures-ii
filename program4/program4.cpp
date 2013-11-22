9f5f247863fbfbff75d09574504939107271e16a
luster <ethan.lusterman@gmail.com>
2013-11-22 16:21:28 -0500

    started parsing input file

--- program4/program4.cpp
+++ program4/program4.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
 
+
+int main() {
+    string inFilename;
+    cout << "Enter name of input file: ";
+    cin >> inFilename;
+    ifstream in(inFilename.c_str());
+
+    string line, a, b, c;
+    while (in.good()) {
+        getline(in,a);
+        getline(in,b);
+        getline(in,c);
+
+        cout << a << " " << b << " " << c << endl;
+    }
 
     return 0;
 }
