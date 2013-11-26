a8c967fb8dda6cd67a0d2dd68e15a2a909658824
luster <ethan.lusterman@gmail.com>
2013-11-25 21:00:02 -0500

    added makefile and updated getting input filestream

--- program4/program4.cpp
+++ program4/program4.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-
-
 int main() {
-    string inFilename;
+    string inFilename, outFilename;
     cout << "Enter name of input file: ";
     cin >> inFilename;
+    cout << "Enter name of output file: ";
+    cin >> outFilename;
     ifstream in(inFilename.c_str());
+    ofstream out(outFilename.c_str());
 
     string line, a, b, c;
-    while (in.good()) {
-        getline(in,a);
-        getline(in,b);
-        getline(in,c);
+    while (getline(in,a) && getline(in,b) && getline(in,c)) {
 
         cout << a << " " << b << " " << c << endl;
+
+        // check if valid merge. write to output filestream properly.
+
     }
 
+    in.close();
+    out.close();
     return 0;
 }
