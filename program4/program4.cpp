56f2f8adcb6a739c8171638272dd032bcc289c74
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-11-27 12:06:33 -0500

    fixed bug, verified instructor test case, added documentation

--- program4/program4.cpp
+++ program4/program4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <cctype>
 
 using namespace std;
 
@@ -9,22 +9,62 @@ using namespace std;
 
 int d[LEN][LEN] = {0};
 
-int main() {
+/*
+ *
+ * Given an input file of the form
+ *
+ *      word_a1
+ *      word_b1
+ *      word_c1
+ *      word_a2
+ *      word_b2
+ *      word_c2
+ *      ...
+ *
+ * determine if the third word C is a valid merge
+ * of the previous two words. C is a valid merge of
+ * A and B if the order of letters is preserved in C.
+ * Think of it like shuffling a deck of cards split into
+ * A and B piles, where you want the shuffled deck C
+ * to preserve the orders of A and B.
+ *
+ * Example of merge:    A: CAT
+ *                      B: dog
+ *                      C: CAdTog
+ *
+ * Example of !merge:   A: CAT
+ *                      B: dog
+ *                      C: CAgoTd
+ *
+ * For a valid merge, the program outputs the merged word
+ * with the letters of word A capitalized to the specified
+ * output filestream. Letters in A are kept as left as possible
+ * in the case that multiple valid merges are possible.
+ *
+ * Example:             A: AA
+ *                      B: ba
+ *                      C: abaa -> AbAa, not AbaA
+ *
+ * In the case of an invalid merge, the line
+ *
+ *      *** NOT A MERGE ***
+ *
+ * is written to the output filestream.
+ *
+ */
+ int main() {
     string inFilename, outFilename;
     cout << "Enter name of input file: ";
     cin >> inFilename;
-//    cout << "Enter name of output file: ";
-//    cin >> outFilename;
+    cout << "Enter name of output file: ";
+    cin >> outFilename;
     ifstream in(inFilename.c_str());
-//    ofstream out(outFilename.c_str());
+    ofstream out(outFilename.c_str());
 
-    bool isMerge = true;
-
-    string line, a, b, c;
+    string a, b, c;
     while (getline(in,a) && getline(in,b) && getline(in,c)) {
-//        cout << a << endl; cout << b << endl; cout << c << endl;
 
-        // initialize
+        // initialize matrix
         for (int i=0; i<LEN; i++) {
             for (int j=0; j<LEN; j++) {
                 d[i][j] = 0;
@@ -32,92 +72,49 @@ int main() {
         }
         d[0][0] = 1;
 
-        int i=0;
-        int j=0;
-        int k=0;
-
         int la = a.length();
         int lb = b.length();
         int lc = c.length();
 
-        isMerge = true;
+        if (lc != la + lb) {
+            out << "*** NOT A MERGE ***" << endl;
+            continue;
+        }
 
+        // fill matrix to get merge paths
         for (int x=0; x<=la; x++) {
             for (int y=0; y<=lb; y++) {
+
                 if (d[x][y] == 1) {
+
                     if (c[x+y] == a[x])
                         d[x+1][y] = 1;
                     if (c[x+y] == b[y])
                         d[x][y+1] = 1;
-                } else {
-                    //break;
+
                 }
-
-            // print matrix
-//            for (int p=0; p<la+1; p++) {
-//                for (int q=0; q<lb+1; q++) {
-//                    cout << d[p][q] << " ";
-//                } cout << endl;
-//            }
-//            cout << endl;
-
             }
         }
+
+        // merge condition, print string to output
         if (d[la][lb] == 1) {
-            cout << "MERGE" << endl;
+            int i=la;
+            int j=lb;
+            while (i>0 && j>=0) {
+                if (d[i][j] and (j == 0 or !d[i][j-1])) {
+                    c[i+j-1] = toupper(c[i+j-1]);
+                    i--;
+                } else {
+                    j--;
+                }
+            }
+            out << c << endl;
         } else {
-            cout << "NOT A MERGE" << endl;
+            out << "*** NOT A MERGE ***" << endl;
         }
     }
 
-
-
-//        while (isMerge) {
-//            // print matrix to test
-//            for (int x=0; x<la+1; x++) {
-//                for (int y=0; y<lb+1; y++) {
-//                    cout << d[x][y] << " ";
-//                } cout << endl;
-//            }
-//            cout << endl;
-//
-//            if (lc != la + lb) {
-//                cout << "*** NOT A MERGE ***" << endl;
-//                isMerge = false;
-//            }
-//            if (c[k] == a[i] && c[k] == b[j]) {
-//                d[i+1][j] = 1;
-//                d[i][j+1] = 1;
-//                i++;
-//                j++;
-//                k++;
-//            } else if (c[k] == a[i]) {
-//                d[++i][j] = 1;
-//                k++;
-//            } else if (c[k] == b[j]) {
-//                d[i][++j] = 1;
-//                k++;
-//            } else {
-//                cout << "*** NOT A MERGE ***" << endl;
-//                isMerge = false;
-//            }
-//            cout << i << j << k << endl;
-//
-//            if (d[la][lb] == 1) {
-//                for (int x=0; x<la+1; x++) {
-//                    for (int y=0; y<lb+1; y++) {
-//                        cout << d[x][y] << " ";
-//                    } cout << endl;
-//                }
-//                cout << "*** MERGE!! ***" << endl;
-//                break;
-//            }
-
-
-      //  }
-    //}
-
     in.close();
-//    out.close();
+    out.close();
     return 0;
 }
